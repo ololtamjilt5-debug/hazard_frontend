d356437383fac6fb12cb1e43f344d5bb04318b49
ololtamjilt5-debug <ololtamjilt5@gmail.com>
2026-02-12 23:36:46 +0800

    Хэрэглэгчийн хэсэг гэсэн тщкстийг жижигрүүлсэн

--- src/components/layout/UserHeader.jsx
+++ src/components/layout/UserHeader.jsx
@@ -4,7 +4,7 @@ const UserHeader = () => (
   <div className="flex items-center justify-center bg-ololt-rgbgreen p-3 shadow-md">
     <div className="flex items-center gap-2"> {/* Gap ашиглавал зай нь илүү жигд байна */}
       <CircleUserRound className='text-white w-6 h-6' strokeWidth={1.5} />
-      <h1 className="text-white text-lg font-condensed uppercase">
+      <h1 className="text-white text-md font-condensed uppercase">
         Хэрэглэгчийн хэсэг
       </h1>
     </div>
