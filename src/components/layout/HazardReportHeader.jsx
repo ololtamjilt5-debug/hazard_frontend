57044855ddc9de7ea2a68339810cf8c06654e9e1
ololtamjilt5-debug <ololtamjilt5@gmail.com>
2026-02-13 00:15:13 +0800

    Аюулыг мэдээлэх хуудасны толгой хэсгийг зассан

--- src/components/layout/HazardReportHeader.jsx
+++ src/components/layout/HazardReportHeader.jsx
@@ -3,8 +3,8 @@ import { TriangleAlert } from 'lucide-react';
 const HazardReportHeader = () => (
   <div className="flex items-center justify-center bg-ololt-rgbgreen p-3 shadow-md">
     <div className="flex items-center gap-2"> {/* Gap ашиглавал зай нь илүү жигд байна */}
-      <TriangleAlert className='text-white w-6 h-6' strokeWidth={1.5} />
-      <h1 className="text-white text-lg font-condensed uppercase">
+      <TriangleAlert className='text-white w-6 h-6' strokeWidth={2} />
+      <h1 className="text-white text-md font-condensed uppercase">
         Аюулыг мэдээлэх хуудас
       </h1>
     </div>
